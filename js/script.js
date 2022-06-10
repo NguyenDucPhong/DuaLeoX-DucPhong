c0b1c424008a1083cac01d650e080e9032046d4c
NguyenDucPhong <pn463187@gmail.com>
2022-06-11 00:42:50 +0700

    Dang lam do blog

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
 $(document).ready(function(){
-    //HERO SLIDER
-    $('#list-card').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
+    $('#list-fruit').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
         dots: false,
-        smartSpeed: 1000,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:false,
-        navText: ['PREV','NEXT'],
         responsive:{
             0:{
-                nav: false,
+                items:1
             },
-            768:{
-                nav: true,
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+    $('#list-vegetable').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
             }
         }
     })
