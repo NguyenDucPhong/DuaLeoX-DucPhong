26ecce998ec8c1c462e8213ff4447bfe8b8e0809
NguyenDucPhong <pn463187@gmail.com>
2022-06-10 17:19:03 +0700

    xong phan slider

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    //HERO SLIDER
+    $('#list-card').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        navText: ['PREV','NEXT'],
+        responsive:{
+            0:{
+                nav: false,
+            },
+            768:{
+                nav: true,
+            }
+        }
+    })
+})
