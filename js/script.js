a1f95c53d857a86b05c3c4893a6973b43dbb93ab
NguyenDucPhong <pn463187@gmail.com>
2022-06-14 00:54:26 +0700

    Dang lam do phan login admin

--- js/script.js
+++ js/script.js
@@ -71,3 +71,17 @@ $(document).ready(function(){
         }
     })
 })
+
+let navLink = document.querySelectorAll('.nav-pills .navlink');
+let listCard = document.querySelectorAll('.list-card');
+
+for(let i=0;i<navLink.length;i++){
+    navLink[i].addEventListener('click', function(){
+        for(let j=0;j<navLink.length;j++){
+            navLink[j].classList.remove('active');
+            listCard[j].classList.remove('active');
+        }
+        navLink[i].classList.add('active');
+        listCard[i].classList.add('active');
+    })
+}
